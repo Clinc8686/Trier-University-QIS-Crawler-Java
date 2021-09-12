e7557becadda92ee2571b459777e9af694bce3f8
Clinc8686 <37739800+Clinc8686@users.noreply.github.com>
2021-09-13 01:58:39 +0200

    Added password decryption wiht auth-key and POST-Request

--- Hochschul_Crawler.java
+++ Hochschul_Crawler.java
@@ -1,41 +1,100 @@
 import com.gargoylesoftware.htmlunit.*;
 import com.gargoylesoftware.htmlunit.html.*;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
 
+import javax.crypto.Cipher;
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.StringReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.URLDecoder;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Base64;
 import java.util.Properties;
 import java.util.Scanner;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
-
-//Semesterdaten mit in der Registry abspeichern
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Hochschul_Crawler {
     private static String username = "";
     private static String password = "";
+    private static SecretKeySpec secretKey;
+    private static String secret_Key = "";
+    private static byte[] key;
 
     public static void main(String[] args) throws IOException, InterruptedException {
         Hochschul_Crawler hsc = new Hochschul_Crawler();
+        hsc.getlogin();
+        secret_Key = read_secret_key();
+        password = decrypt(password, secret_Key);
+        add_Shutdown_Hook();
         checkOnUpdates();
         while(true) {
             LocalDateTime localdatetime = LocalDateTime.now();
             if (!(localdatetime.getHour() >= 0 && localdatetime.getHour() <= 5)) {
-                hsc.getlogin();
+
                 hsc.read_QIS();
             }
             Thread.sleep(900000);
         }
         //System.exit(0);
+    }
+
+    private static void add_Shutdown_Hook() {
+        Runtime.getRuntime().addShutdownHook(
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            PrintWriter key_writer = new PrintWriter(System.getProperty("user.dir") + "\\tmp_key.txt");
+                            key_writer.println(secret_Key);
+                            key_writer.close();
+                        } catch (Exception e) {
+                        }
+                    }
+                }));
+    }
+
+    private static String read_secret_key() {
+        String secret_key = "";
+        try {
+            File key_reader = null;
+            Scanner key_scanner = null;
+            String path = "";
+
+            path = Hochschul_Crawler.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+            path = path.substring(0, path.lastIndexOf("/") + 1)+"\\tmp_key.txt";
+            path = URLDecoder.decode(path, "UTF-8");
+
+            key_reader = new File(path);
+            key_scanner = new Scanner(key_reader);
+            secret_key = key_scanner.nextLine();
+            key_scanner.close();
+
+            if(!(key_reader.delete())) {
+                throw new IllegalAccessException("Konnte Datei nicht löschen");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(new JFrame(), "Hier hat etwas nicht funktioniert!\nBitte Installiere das Programm über den Installer noch mal neu. Fehler: ReadSeFile", "Hochschul-Crawler",
+                    JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
+        }
+        return secret_key;
     }
 
     private static void checkOnUpdates() {
@@ -48,9 +107,10 @@ public class Hochschul_Crawler {
                 sendWinNotification("Hochschul-Crawler", "Es ist ein Update verfügbar!\nÜber den Installer kannst du das neue Update installieren.", "");
             }
         } catch (IOException e) {
-
+            JOptionPane.showMessageDialog(new JFrame(), "Fehler!\nIch konnte keine Update-Windows Benachrichtigung senden!.", "Hochschul-Crawler",
+                    JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
         }
-
     }
 
     public static void sendWinNotification(String title, String subtitle, String pathToIcon) {
@@ -63,7 +123,10 @@ public class Hochschul_Crawler {
             mainTrayIcon.displayMessage(title,  subtitle, TrayIcon.MessageType.NONE);
         }
         catch (Exception e) {
-            e.printStackTrace();
+            //e.printStackTrace();
+            JOptionPane.showMessageDialog(new JFrame(), "Fehler!\nIch konnte keine Windows Benachrichtigung senden!.", "Hochschul-Crawler",
+                    JOptionPane.ERROR_MESSAGE);
+            System.exit(0);
         }
     }
 
@@ -115,12 +178,11 @@ public class Hochschul_Crawler {
             grades = g.click();
             webClient.close();
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(new JFrame(), "Hier hat etwas nicht funktioniert!\nVielleicht sind deine Anmeldedaten falsch?", "Hochschul-Crawler",
+            JOptionPane.showMessageDialog(new JFrame(), "Hier hat etwas nicht funktioniert!\nKonnte keine Daten aus dem QIS abrufen!", "Hochschul-Crawler",
                     JOptionPane.ERROR_MESSAGE);
             System.exit(0);
         }
 
-        //System.out.println(grades.asNormalizedText());
         String grade_page_s = grades.asNormalizedText();
         BufferedReader reader = new BufferedReader(new StringReader(grade_page_s));
 
@@ -140,9 +202,14 @@ public class Hochschul_Crawler {
         String mod = "";
         Preferences prefs = Preferences.userRoot().node("Hochschul-Scraper");
         while ((s = reader.readLine()) != null) {
+
             if(((s.contains("BE") || s.contains("NB") || s.contains("NE")) && s.contains(semester)) && (!(mod.contains("PV") || mod.contains("Studienleistung")))) {
                 mod = mod.replace("\t", " ");
                 mod = mod.replace("  ", " ");
+
+                s = s.replace("\t", " ");
+                s = s.replace("  ", " ");
+                String[] s_splitted = s.split("\\s+");
 
                 String mod_reg = mod;
                 mod_reg = mod_reg.replace("ä", "ae");
@@ -155,20 +222,75 @@ public class Hochschul_Crawler {
                 boolean pref_exists = false;
                 try {
                     for (String preference : prefs.keys()) {
-                        if (preference.equals(mod_reg)) {
+                        if (preference.equals(mod_reg + "|" + s_splitted[1] + " " + s_splitted[2])) {
                             pref_exists = true;
                         }
                     }
                 } catch (BackingStoreException backingStoreException) {
 
                 }
+
                 if (pref_exists == false) {
-                    prefs.put(mod_reg, "1");
+                    prefs.put(mod_reg + "|" + s_splitted[1] + " " + s_splitted[2], "1");
                     sendWinNotification("QIS-Benachrichtigung", "Für \"" + mod + "\" sind die Noten im QIS eingetragen!", "favicon_FHTRIER.jpg");
+
+                    try {
+                        String jsonInputString = "{ \"modul\": \"" + mod_reg + "|" + s_splitted[1] + " " + s_splitted[2] + "\" }";
+                        System.out.println("sende jetzt: " + jsonInputString);
+                        StringEntity entity = new StringEntity(jsonInputString, ContentType.APPLICATION_FORM_URLENCODED);
+                        HttpClient httpClient = HttpClientBuilder.create().build();
+                        HttpPost request = new HttpPost("https://mariolampert.de/HS-Bot/API/index.php");
+                        request.setEntity(entity);
+                        HttpResponse response = httpClient.execute(request);
+                    } catch (Exception e) {
+                        JOptionPane.showMessageDialog(new JFrame(), "Fehler!\nIch konnte keine Nachricht an den Server senden!.", "Hochschul-Crawler",
+                                JOptionPane.ERROR_MESSAGE);
+                        System.exit(0);
+                    }
                 }
             }
             mod = s;
         }
         reader.close();
+    }
+
+    public static void setKey(String myKey)
+    {
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes("UTF-8");
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, "AES");
+        }
+        catch (NoSuchAlgorithmException e) {
+            //e.printStackTrace();
+            JOptionPane.showMessageDialog(new JFrame(), "Hier hat etwas nicht funktioniert!\nBitte Installiere das Programm über den Installer noch mal neu. Fehler: NSAE", "Hochschul-Crawler",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+        catch (UnsupportedEncodingException e) {
+            //e.printStackTrace();
+            JOptionPane.showMessageDialog(new JFrame(), "Hier hat etwas nicht funktioniert!\nBitte Installiere das Programm über den Installer noch mal neu. Fehler: UEE", "Hochschul-Crawler",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public static String decrypt(String strToDecrypt, String secret)
+    {
+        try
+        {
+            setKey(secret);
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        }
+        catch (Exception e)
+        {
+            JOptionPane.showMessageDialog(new JFrame(), "Hier hat etwas nicht funktioniert!\nBitte Installiere das Programm über den Installer noch mal neu. Fehler: decryption", "Hochschul-Crawler",
+                    JOptionPane.ERROR_MESSAGE);
+            //System.out.println("Error while decrypting: " + e.toString());
+        }
+        return null;
     }
 }
